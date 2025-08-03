596130cd4ad6ba2b67386505377e81cd073fe672
Benjamin Braunfels <inf3068@hs-worms.de>
2025-08-04 00:38:49 +0200

    Add conditional guards to prevent macro redefinition warnings

--- src/util.h
+++ src/util.h
@@ -1,13 +1,23 @@
-#ifndef UTIL_H
+﻿#ifndef UTIL_H
 #define UTIL_H
 
+#ifndef htons
 #define htons(x) ( ((x)<<8) | (((x)>>8)&0xFF) )
-#define ntohs(x) htons(x)
+#endif
 
+#ifndef ntohs
+#define ntohs(x) htons(x)
+#endif
+
+#ifndef htonl
 #define htonl(x) ( ((x)<<24 & 0xFF000000UL) | \
                    ((x)<< 8 & 0x00FF0000UL) | \
                    ((x)>> 8 & 0x0000FF00UL) | \
-                   ((x)>>24 & 0x000000FFUL) )
+                   ((x)>>24 & 0x000000FIUL) )
+#endif
+
+#ifndef ntohl
 #define ntohl(x) htonl(x)
+#endif
 
 #endif
